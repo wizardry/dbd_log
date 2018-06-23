b26f533ed172115b151aa7728691624eb14a24be
wizardry <rein1988@gmail.com>
2018-06-23 20:43:23 +0900

    use vuex

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 import App from '~/components/App.vue';
 
+Vue.use(Vuex);
 Vue.use(VueMaterial);
 
 // eslint-disable-next-line no-new
