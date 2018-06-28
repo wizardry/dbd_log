fcb164a9bf7f552da65569133b17c74084dfdec8
wizardry <rein1988@gmail.com>
2018-06-28 18:42:43 +0900

    キャラクターデータとパークデータを持ってきた

--- src/api/mock/parks.js
+++ src/api/mock/parks.js
@@ -1,27 +1,609 @@
 export default {
   parks: [{
+    id: 0,
+    type: "survivor",
+    name_en: "Light weight",
+    name_jp: "身軽",
+    teachable: false,
+  }, {
     id: 1,
-    name_en: 'Light weight',
-    name_jp: '身軽',
-    type: 'survivor',
+    type: "survivor",
+    name_en: "Plunderer's Instinct",
+    name_jp: "こそ泥の本能",
     teachable: false,
   }, {
     id: 2,
-    name_en: 'Plunderer\'s Instinct',
-    name_jp: 'こそ泥の本能',
-    type: 'survivor',
+    type: "survivor",
+    name_en: "Small Game",
+    name_jp: "小さな獲物",
     teachable: false,
   }, {
     id: 3,
-    name_en: 'Small Game',
-    name_jp: '小さな獲物',
-    type: 'survivor',
+    type: "survivor",
+    name_en: "Slippery Meat",
+    name_jp: "滑りやすい肉体",
     teachable: false,
   }, {
     id: 4,
-    name_en: 'Slippery Meat',
-    name_jp: '滑りやすい肉体',
-    type: 'survivor',
+    type: "survivor",
+    name_en: "Deja Vu",
+    name_jp: "デジャヴ",
     teachable: false,
-  }],
+  }, {
+    id: 5,
+    type: "survivor",
+    name_en: "Resilience",
+    name_jp: "逆境魂",
+    teachable: false,
+  }, {
+    id: 6,
+    type: "survivor",
+    name_en: "This Is Not Happening",
+    name_jp: "痛みも気から",
+    teachable: false,
+  }, {
+    id: 7,
+    type: "survivor",
+    name_en: "Kindred",
+    name_jp: "血族",
+    teachable: false,
+  }, {
+    id: 8,
+    type: "survivor",
+    name_en: "We'll Make It",
+    name_jp: "きっとやり遂げる",
+    teachable: false,
+  }, {
+    id: 9,
+    type: "survivor",
+    name_en: "Hope",
+    name_jp: "希望",
+    teachable: false,
+  }, {
+    id: 10,
+    type: "survivor",
+    name_en: "No One Left Behind",
+    name_jp: "誰も見捨てはしない",
+    teachable: false,
+  }, {
+    id: 11,
+    type: "survivor",
+    name_en: "Premonition",
+    name_jp: "予感",
+    teachable: false,
+  }, {
+    id: 12,
+    type: "survivor",
+    name_en: "Spine Chill",
+    name_jp: "凍りつく背筋",
+    teachable: false,
+  }, {
+    id: 13,
+    type: "survivor",
+    name_en: "Dark Sense",
+    name_jp: "闇の感覚",
+    teachable: false,
+  }, {
+    id: 14,
+    type: "survivor",
+    name_en: "Bond",
+    name_jp: "絆",
+    teachable: true,
+  }, {
+    id: 15,
+    type: "survivor",
+    name_en: "Prove Thyself",
+    name_jp: "有能の証明",
+    teachable: true,
+  }, {
+    id: 16,
+    type: "survivor",
+    name_en: "Leader",
+    name_jp: "リーダー",
+    teachable: true,
+  }, {
+    id: 17,
+    type: "survivor",
+    name_en: "Quick & Quiet",
+    name_jp: "素早く静かに",
+    teachable: true,
+  }, {
+    id: 18,
+    type: "survivor",
+    name_en: "Sprint Burst",
+    name_jp: "全力疾走",
+    teachable: true,
+  }, {
+    id: 19,
+    type: "survivor",
+    name_en: "Adrenaline",
+    name_jp: "アドレナリン",
+    teachable: true,
+  }, {
+    id: 20,
+    type: "survivor",
+    name_en: "Iron Will",
+    name_jp: "鋼の意志",
+    teachable: true,
+  }, {
+    id: 21,
+    type: "survivor",
+    name_en: "Calm Spirit",
+    name_jp: "魂の平穏",
+    teachable: true,
+  }, {
+    id: 22,
+    type: "survivor",
+    name_en: "Saboteur",
+    name_jp: "サボタージュ",
+    teachable: true,
+  }, {
+    id: 23,
+    type: "survivor",
+    name_en: "Empathy",
+    name_jp: "共感",
+    teachable: true,
+  }, {
+    id: 24,
+    type: "survivor",
+    name_en: "Botany Knowledge",
+    name_jp: "植物学の知識",
+    teachable: true,
+  }, {
+    id: 25,
+    type: "survivor",
+    name_en: "Self Care",
+    name_jp: "セルフケア",
+    teachable: true,
+  }, {
+    id: 26,
+    type: "survivor",
+    name_en: "Balanced Landing",
+    name_jp: "スマートな着地",
+    teachable: true,
+  }, {
+    id: 27,
+    type: "survivor",
+    name_en: "Urban Evasion",
+    name_jp: "都会の逃走術",
+    teachable: true,
+  }, {
+    id: 28,
+    type: "survivor",
+    name_en: "Streetwise",
+    name_jp: "都会の生存術",
+    teachable: true,
+  }, {
+    id: 29,
+    type: "survivor",
+    name_en: "Sole Survivor",
+    name_jp: "唯一の生存者",
+    teachable: true,
+  }, {
+    id: 30,
+    type: "survivor",
+    name_en: "Object of Obsession",
+    name_jp: "執念の対象",
+    teachable: true,
+  }, {
+    id: 31,
+    type: "survivor",
+    name_en: "Decisive Strike",
+    name_jp: "決死の一撃",
+    teachable: true,
+  }, {
+    id: 32,
+    type: "survivor",
+    name_en: "Open-Handed",
+    name_jp: "手札公開",
+    teachable: true,
+  }, {
+    id: 33,
+    type: "survivor",
+    name_en: "Up the Ante",
+    name_jp: "賭け金のレイズ",
+    teachable: true,
+  }, {
+    id: 34,
+    type: "survivor",
+    name_en: "Ace in the Hole",
+    name_jp: "最後の切り札",
+    teachable: true,
+  }, {
+    id: 35,
+    type: "survivor",
+    name_en: "Left Behind",
+    name_jp: "置き去りにされた者",
+    teachable: true,
+  }, {
+    id: 36,
+    type: "survivor",
+    name_en: "Borrowed Time",
+    name_jp: "与えられた猶予",
+    teachable: true,
+  }, {
+    id: 37,
+    type: "survivor",
+    name_en: "Unbreakable",
+    name_jp: "不滅",
+    teachable: true,
+  }, {
+    id: 38,
+    type: "survivor",
+    name_en: "Technician",
+    name_jp: "テクニシャン",
+    teachable: true,
+  }, {
+    id: 39,
+    type: "survivor",
+    name_en: "Lithe",
+    name_jp: "しなやか",
+    teachable: true,
+  }, {
+    id: 40,
+    type: "survivor",
+    name_en: "Alert",
+    name_jp: "警戒",
+    teachable: true,
+  }, {
+    id: 41,
+    type: "survivor",
+    name_en: "We're Gonna Live Forever",
+    name_jp: "ずっと一緒だ",
+    teachable: true,
+  }, {
+    id: 42,
+    type: "survivor",
+    name_en: "Dead Hard",
+    name_jp: "デッド・ハード",
+    teachable: true,
+  }, {
+    id: 43,
+    type: "survivor",
+    name_en: "No Mither",
+    name_jp: "弱音はナシだ",
+    teachable: true,
+  }, {
+    id: 44,
+    type: "survivor",
+    name_en: "Wake Up",
+    name_jp: "目を覚ませ！",
+    teachable: true,
+  }, {
+    id: 45,
+    type: "survivor",
+    name_en: "Phar/macy",
+    name_jp: "調剤学",
+    teachable: true,
+  }, {
+    id: 46,
+    type: "survivor",
+    name_en: "Vigil",
+    name_jp: "不寝番",
+    teachable: true,
+  }, {
+    id: 47,
+    type: "survivor",
+    name_en: "Tenacity",
+    name_jp: "執念",
+    teachable: true,
+  }, {
+    id: 48,
+    type: "survivor",
+    name_en: "Detective's Hunch",
+    name_jp: "刑事の直感",
+    teachable: true,
+  }, {
+    id: 49,
+    type: "survivor",
+    name_en: "Stake Out",
+    name_jp: "張り込み",
+    teachable: true,
+  }, {
+    id: 50,
+    type: "survivor",
+    name_en: "Dance With Me",
+    name_jp: "私と踊ろう",
+    teachable: true,
+  }, {
+    id: 51,
+    type: "survivor",
+    name_en: "Windows Of Opportunity",
+    name_jp: "好機の窓",
+    teachable: true,
+  }, {
+    id: 52,
+    type: "survivor",
+    name_en: "Boil Over",
+    name_jp: "ボイルオーバー",
+    teachable: true,
+  }, {
+    id: 53,
+    type: "killer",
+    name_en: "Sloppy Butcher",
+    name_jp: "ずさんな肉屋",
+    teachable: false
+  }, {
+    id: 54,
+    type: "killer",
+    name_en: "Deerstalker",
+    name_jp: "鹿猟師",
+    teachable: false
+  }, {
+    id: 55,
+    type: "killer",
+    name_en: "Iron Grasp",
+    name_jp: "鉄の握力",
+    teachable: false
+  }, {
+    id: 56,
+    type: "killer",
+    name_en: "Hex:Thrill of the Hunt",
+    name_jp: "呪術:狩りの興奮",
+    teachable: false
+  }, {
+    id: 57,
+    type: "killer",
+    name_en: "Unrelenting",
+    name_jp: "無慈悲",
+    teachable: false
+  }, {
+    id: 58,
+    type: "killer",
+    name_en: "Monstrous Shrine",
+    name_jp: "異形の祭壇",
+    teachable: false
+  }, {
+    id: 59,
+    type: "killer",
+    name_en: "Distressing",
+    name_jp: "苦悶の根源",
+    teachable: false
+  }, {
+    id: 60,
+    type: "killer",
+    name_en: "Spies From The Shadows",
+    name_jp: "影の中の密偵",
+    teachable: false
+  }, {
+    id: 61,
+    type: "killer",
+    name_en: "Whispers",
+    name_jp: "囁き",
+    teachable: false
+  }, {
+    id: 62,
+    type: "killer",
+    name_en: "Insidious",
+    name_jp: "狡猾",
+    teachable: false
+  }, {
+    id: 63,
+    type: "killer",
+    name_en: "Bitter Murmur",
+    name_jp: "憎悪の呟き",
+    teachable: false
+  }, {
+    id: 64,
+    type: "killer",
+    name_en: "Hex:No One Escapes Death",
+    name_jp: "呪術:誰も死から逃れられない",
+    teachable: false
+  }, {
+    id: 65,
+    type: "killer",
+    name_en: "Unnerving Presence",
+    name_jp: "不安の元凶",
+    teachable: true
+  }, {
+    id: 66,
+    type: "killer",
+    name_en: "Brutal Strength",
+    name_jp: "野蛮な力",
+    teachable: true
+  }, {
+    id: 67,
+    type: "killer",
+    name_en: "Agitation",
+    name_jp: "興奮",
+    teachable: true
+  }, {
+    id: 68,
+    type: "killer",
+    name_en: "Predator",
+    name_jp: "捕食者",
+    teachable: true
+  }, {
+    id: 69,
+    type: "killer",
+    name_en: "Bloodhound",
+    name_jp: "血の追跡者",
+    teachable: true
+  }, {
+    id: 70,
+    type: "killer",
+    name_en: "Shadowborn",
+    name_jp: "闇より出でし者",
+    teachable: true
+  }, {
+    id: 71,
+    type: "killer",
+    name_en: "Enduring",
+    name_jp: "不屈",
+    teachable: true
+  }, {
+    id: 72,
+    type: "killer",
+    name_en: "Lightborn",
+    name_jp: "光より出でし者",
+    teachable: true
+  }, {
+    id: 73,
+    type: "killer",
+    name_en: "Tinkerer",
+    name_jp: "ガラクタいじり",
+    teachable: true
+  }, {
+    id: 74,
+    type: "killer",
+    name_en: "Stridor",
+    name_jp: "喘",
+    teachable: true
+  }, {
+    id: 75,
+    type: "killer",
+    name_en: "Thanatophobia",
+    name_jp: "死恐怖症",
+    teachable: true
+  }, {
+    id: 76,
+    type: "killer",
+    name_en: "A Nurse's Calling",
+    name_jp: "看護婦の使命",
+    teachable: true
+  }, {
+    id: 77,
+    type: "killer",
+    name_en: "Save the Best for Last",
+    name_jp: "最後のお楽しみ",
+    teachable: true
+  }, {
+    id: 78,
+    type: "killer",
+    name_en: "Play with your Food",
+    name_jp: "弄ばれる獲物",
+    teachable: true
+  }, {
+    id: 79,
+    type: "killer",
+    name_en: "Dying Light",
+    name_jp: "消えゆく灯",
+    teachable: true
+  }, {
+    id: 80,
+    type: "killer",
+    name_en: "Hex:The Third Seal",
+    name_jp: "呪術:第三の封印",
+    teachable: true
+  }, {
+    id: 81,
+    type: "killer",
+    name_en: "Hex:Ruin",
+    name_jp: "呪術:破滅",
+    teachable: true
+  }, {
+    id: 82,
+    type: "killer",
+    name_en: "Hex:Devour Hope",
+    name_jp: "呪術:貪られる希望",
+    teachable: true
+  }, {
+    id: 83,
+    type: "killer",
+    name_en: "Overwhelming Presence",
+    name_jp: "圧倒的存在感",
+    teachable: true
+  }, {
+    id: 84,
+    type: "killer",
+    name_en: "Monitor & Abuse",
+    name_jp: "観察&虐待",
+    teachable: true
+  }, {
+    id: 85,
+    type: "killer",
+    name_en: "Overcharge",
+    name_jp: "オーバーチャージ",
+    teachable: true
+  }, {
+    id: 86,
+    type: "killer",
+    name_en: "Beast Of Prey",
+    name_jp: "猛獣",
+    teachable: true
+  }, {
+    id: 87,
+    type: "killer",
+    name_en: "Territorial Imperative",
+    name_jp: "縄張り意識",
+    teachable: true
+  }, {
+    id: 88,
+    type: "killer",
+    name_en: "Hex:Huntress Lullaby",
+    name_jp: "呪術:女狩人の子守唄",
+    teachable: true
+  }, {
+    id: 89,
+    type: "killer",
+    name_en: "Knock Out",
+    name_jp: "ノックアウト",
+    teachable: true
+  }, {
+    id: 90,
+    type: "killer",
+    name_en: "Barbecue & Chili",
+    name_jp: "バーベキュー&チリ",
+    teachable: true
+  }, {
+    id: 91,
+    type: "killer",
+    name_en: "Franklin's Demise",
+    name_jp: "フランクリンの悲劇",
+    teachable: true
+  }, {
+    id: 92,
+    type: "killer",
+    name_en: "Fire up",
+    name_jp: "ファイヤー・アップ",
+    teachable: true
+  }, {
+    id: 93,
+    type: "killer",
+    name_en: "Remember me",
+    name_jp: "リメンバーミー",
+    teachable: true
+  }, {
+    id: 94,
+    type: "killer",
+    name_en: "Blood Warden",
+    name_jp: "血の番人",
+    teachable: true
+  }, {
+    id: 95,
+    type: "killer",
+    name_en: "Hangman's Trick",
+    name_jp: "処刑人の妙技",
+    teachable: true
+  }, {
+    id: 96,
+    type: "killer",
+    name_en: "Surveillance",
+    name_jp: "監視",
+    teachable: true
+  }, {
+    id: 97,
+    type: "killer",
+    name_en: "Make Your Choice",
+    name_jp: "選択は君次第だ",
+    teachable: true
+  }, {
+    id: 98,
+    type: "killer",
+    name_en: "Bamboozle",
+    name_jp: "まやかし",
+    teachable: true
+  }, {
+    id: 99,
+    type: "killer",
+    name_en: "Coulrophobia",
+    name_jp: "ピエロ恐怖症",
+    teachable: true
+  }, {
+    id: 100,
+    type: "killer",
+    name_en: "Pop Goes The Weasel",
+    name_jp: "イタチが飛び出した",
+    teachable: true
+  }]
 };
