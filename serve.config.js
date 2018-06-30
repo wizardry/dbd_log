ede11de286490d384f0074236ae7743055bc1ac1
wizardry <rein1988@gmail.com>
2018-06-30 23:46:31 +0900

    gh-pages init

--- serve.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  host: 'localhost',
-  port: 8080,
-  content: ['./public', './src/html/'],
-};
--- specs/__test__/components/Smaple.vue.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import Sample from '~/components/Sample.vue';
-
-describe('Sample.vue component', () => {
-  it('shows Hello, World!', () => {
-    const wrapper = shallowMount(Sample);
-    expect(wrapper.text()).toBe('Hello, World!');
-  });
-});
