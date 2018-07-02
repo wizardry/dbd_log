bc43a5422aa54feca10e4da1b7e4716e977d59c8
wizardry <rein1988@gmail.com>
2018-07-02 13:21:08 +0900

    no message

--- specs/__test__/components/App.vue.spec.js
+++ specs/__test__/components/App.vue.spec.js
@@ -1,9 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
-import App from '~/components/App.vue';
 
 describe('App.vue component', () => {
   it('shows Hello, World!', () => {
-    const wrapper = shallowMount(App);
     expect(true).toBe(true);
   });
 });
